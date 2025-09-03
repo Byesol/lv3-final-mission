6e66c3696ecee0768bb849e7cda2b756f8800872
byesol <bsj5967@naver.com>
2025-09-03 17:03:46 +0900

    fix: 스웨거 예시 추ㅏ가

--- src/main/java/library/book/controller/BookController.java
+++ src/main/java/library/book/controller/BookController.java
@@ -1,5 +1,7 @@
 package library.book.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import library.book.dto.BookResponse;
 import library.book.infrastructure.BookApiClient;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +18,10 @@ public class BookController {
         this.bookApiClient = bookApiClient;
     }
 
+    @Operation(summary = "도서 검색", description = "키워드를 통해 외부 도서 정보를 검색합니다.")
     @GetMapping("/books/search")
-    public ResponseEntity<BookResponse> searchBooks(@RequestParam("query") String query) {
+    public ResponseEntity<BookResponse> searchBooks(
+            @Parameter(description = "검색할 키워드", required = true, example = "자바") @RequestParam("query") String query) {
         BookResponse response = bookApiClient.searchBook(query);
         return ResponseEntity.ok(response);
     }
