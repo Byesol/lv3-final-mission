118cb575661bcc2c18435dd81d2efaae5a587a49
byesol <bsj5967@naver.com>
2025-09-03 16:25:14 +0900

    chore: lv2 저장소에서 lv3 저장소로 변경

---
+++ src/main/java/library/FinalMissionApplication.java
@@ -0,0 +1,14 @@
+package library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalMissionApplication.class, args);
+    }
+
+}
+
